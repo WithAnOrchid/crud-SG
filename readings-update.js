505e4e4751f71db9f64a5a962a6050a0fd8082a0
Di Liu <unknowngas@gmail.com>
2017-03-09 21:00:02 -0500

    Disable update, replace it with describeTable()

--- readings-update.js
+++ readings-update.js
@@ -5,23 +5,26 @@ const dynamoDb = new AWS.DynamoDB.DocumentClient();
 
 module.exports = (event, callback) => {
   const data = JSON.parse(event.body);
-  const sensor_id = event.queryStringParameters.sensor_id;
-  const published_at = parseInt(event.queryStringParameters.published_at);
+  //const sensor_id = event.queryStringParameters.sensor_id;
+  //const published_at = parseInt(event.queryStringParameters.published_at);
 
-  data.sensor_id = sensor_id;
-  data.sensor_id = published_at;
-  var timeInMs = Date.now();
-  data.processed_at = timeInMs;
+  //data.sensor_id = sensor_id;
+  //data.sensor_id = published_at;
+  //var timeInMs = Date.now();
+  //data.processed_at = timeInMs;
 
-  const params = {
-    TableName : 'readings',
-    Item: data
+  //const params = {
+  //  TableName : 'readings',
+    //Item: data
+  //};
+    const params = {
+    TableName : 'readings'
   };
 
-  return dynamoDb.put(params, (error, data) => {
+  return dynamoDb.describeTable(params, (error, data) => {
     if (error) {
       callback(error);
     }
-    callback(error, params.Item);
+    callback(error, data);
   });
 };
