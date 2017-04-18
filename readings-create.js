627ba9c20be59160e3c168d484ecc8b16117bb2f
Di Liu <unknowngas@gmail.com>
2017-04-18 11:10:11 -0400

    Modify for TH module

--- readings-create.js
+++ readings-create.js
@@ -12,15 +12,64 @@ module.exports = (event, callback) => {
   data.published_at = Date.now();
   data.processed_at = Date.now();
 
-  const params = {
-    TableName: 'readings',
-    Item: data
-  };
+  // Special case for TH module
+  if(data.sensor_type == "TH")
+  {
+    // Split the single JSON to 2 JSONs
+    data1 = {
+      sensor_id: 'TEMPERATURE' + data.sensor_id,
+      device_id: data.device_id,
+      device_type: data.device_type,
+      sensor_type: 'temperature',
+      sensor_reading: data.temperature_reading,
+      published_at: data.published_at
+    };
+    data2 = {
+      sensor_id: 'HUMIDITY' + data.sensor_id,
+      device_id: data.device_id,
+      device_type: data.device_type,
+      sensor_type: 'humidity',
+      sensor_reading: data.humidity_reading,
+      published_at: data.published_at
+    };
+    
+    var params1 = {
+      TableName: 'readings',
+      Item: data1
+    };
+    var params2 = {
+      TableName: 'readings',
+      Item: data2
+    };
+    
+    dynamoDb.put(params1, function(err, data) {
+      if (err) console.log("Unable to update item. Error: ", JSON.stringify(err, null, 2));
+      else console.log("Updated item succeeded: ", JSON.stringify(data, null, 2));
+            //next() // modify for err handling
+          });
+    return dynamoDb.put(params2, (error, data) => {
+      if (error) {
+        callback(error);
+      }
+      callback(error, params.Item);
+    });
+    
+    
 
-  return dynamoDb.put(params, (error, data) => {
-    if (error) {
-      callback(error);
-    }
-    callback(error, params.Item);
-  });
+  } // end of if
+  else
+  {
+    const params = {
+      TableName: 'readings',
+      Item: data
+    };
+
+    return dynamoDb.put(params, (error, data) => {
+      if (error) {
+        callback(error);
+      }
+      callback(error, params.Item);
+    });
+
+}// end of else
 };
